018e203c5fdd73fac2f2c04cb2b9585c1d91c7cf
viet <misslandthetest@gmail.com>
2023-08-01 15:40:03 +0700

    apiKey.service.js apikey.model.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
-const express =require('express')
+const express =require('express');
+const { apiKey,permission } = require('../auth/checkAuth');
 
 const route= express.Router();
-
+//check APi
+route.use(apiKey) //=> trỏ tới middleware (auth)
+//check permission
+route.use(permission('0000'))
 route.use('/v1/api',require('./access/index'))
-// route.get('',(req,res)=>{
-//     return res.status(200).json({
-//         message:'welcome'
-//     })
-// })
+
+//check permission
 
 module.exports=route
