86eff42ac2c28c67a2ee31593bd5902085e99fe2
viet <misslandthetest@gmail.com>
2023-08-03 16:40:36 +0700

    commit

--- src/controller/access.controller.js
+++ src/controller/access.controller.js
@@ -1,9 +1,12 @@
 const AccessService = require("../services/access.service");
-
+const {OK,CREATED}  = require("../core/success.response")
 class AccessController {
     signUp = async (req,res,next)=>{
 
-            return res.status(201).json(await AccessService.signUp(req.body))
+        new CREATED({
+            message:'Registed',
+            metadata :await AccessService.signUp(req.body)
+        }).send(res)
  
     }
 }
